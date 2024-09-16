e950b64b497a2be7672b4ca8212a17e36d6d43c4
polinakubiedinova <polinakubedino@gmail.com>
2024-09-16 11:29:32 +0300

    first commit

---
+++ laba1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace laba1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length= int.Parse(Console.ReadLine());
+           GenerateRandom(length);
+        }
+        static void GenerateRandom(int length) 
+        {
+            Random random = new Random(3467);
+            for (int i = 0; i < length; i++)
+            {
+                int num1 = random.Next(0, 100);
+                Console.Write($"{num1} |");
+            }
+            
+        }
+    }
+}
---
+++ laba1.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+namespace laba1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            byte[] randomBytes = new byte[4];
+            for (int i = 0; i < length; i++)
+            {
+
+                RandomNumberGenerator.Fill(randomBytes);
+                int randomNumber = BitConverter.ToInt32(randomBytes, 0);
+                Console.Write(randomNumber+"|");
+            }
+        }
+    }
+}
