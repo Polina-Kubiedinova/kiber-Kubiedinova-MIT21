ed6657b399c31622221a5d372c9db0d37732eeb2
polinakubiedinova <polinakubedino@gmail.com>
2024-09-26 17:15:15 +0300

    additional

--- laba2.3/Program.cs
+++ laba2.3/Program.cs
@@ -6,38 +6,47 @@ namespace laba2._3
 {
     internal class Program
     {
-        static string Decrypt(byte[] encryptedData, string key)
+        static string Decrypt(byte[] encryptedData, byte[] key)
         {
             var decryptedBytes = new byte[encryptedData.Length];
-            for (int i = 0; i < encryptedData.Length; i++)
+            for (int i = 0; i < encryptedData.Length; i++)//encryptedData.Length
             {
-                decryptedBytes[i] = (byte)(encryptedData[i] ^ key[i % key.Length]);
+                decryptedBytes[i] = (byte)(encryptedData[i] ^ key[i % key.Length]);//% key.Length
             }
             return Encoding.UTF8.GetString(decryptedBytes);
-        }
-        static IEnumerable<string> GenerateKeys(string charset, int length)
-        {
-            // Рекурсивна генерація всіх комбінацій
-            if (length == 1) return charset.Select(c => c.ToString());
-            return GenerateKeys(charset, length - 1)
-                .SelectMany(t => charset, (t, c) => t + c);
         }
         static void Main(string[] args)
         {
             string encFileName = "C:\\polina\\yniver\\kiber\\laba2.3\\encfile5.dat";
             byte[] encryptedData = File.ReadAllBytes(encFileName);
-            string charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            int keyLength = 5;
-            foreach (var key in GenerateKeys(charset, keyLength))
+            byte[] passwordBytes = Encoding.UTF8.GetBytes("Mit21");
+            byte[] Bytes = new byte[passwordBytes.Length];
+            string decryptedText;
+            int position = 3;
+            for (int i = 5; i < 10; i++)//encryptedData.Length
             {
-                string decryptedText = Decrypt(encryptedData, key);
+                Bytes[i-5] = (byte)(encryptedData[i] ^ passwordBytes[i-5]);//% key.Length
+            }
+            Console.WriteLine(Encoding.UTF8.GetString(Bytes));
+            decryptedText = Decrypt(encryptedData, Bytes);
+            Console.WriteLine(decryptedText);
+            while (position <= encryptedData.Length - passwordBytes.Length)
+            {
+                Console.WriteLine(position);
+                position++;
+                for (int i = 0; i < passwordBytes.Length; i++)//encryptedData.Length
+                {
+                    Bytes[i] = (byte)(encryptedData[i + position-1] ^ passwordBytes[i]);//% key.Length
+                }
+                decryptedText = Decrypt(encryptedData, Bytes);
                 if (decryptedText.Contains("Mit21"))
                 {
-                    Console.WriteLine($"Знайдено ключ: {key}");
+                    Console.WriteLine("Знайдений ключ:");
+                    Console.WriteLine(Encoding.UTF8.GetString(Bytes));
                     Console.WriteLine("Дешифрований текст:");
                     Console.WriteLine(decryptedText);
-                    break;
                 }
+                //position++;
             }
         }
     }
--- laba2.3/obj/Debug/net8.0/laba2.3.AssemblyInfo.cs
+++ laba2.3/obj/Debug/net8.0/laba2.3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("laba2.3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e950b64b497a2be7672b4ca8212a17e36d6d43c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b566e489afaa07215816dba34b084d4dd8d8b9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("laba2.3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("laba2.3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
