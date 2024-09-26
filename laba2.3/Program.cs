5c21cf59f149cf34e196ae821542deed7aa1890e
polinakubiedinova <polinakubedino@gmail.com>
2024-09-26 17:37:29 +0300

    additional2

--- laba2.3/Program.cs
+++ laba2.3/Program.cs
@@ -22,21 +22,12 @@ namespace laba2._3
             byte[] passwordBytes = Encoding.UTF8.GetBytes("Mit21");
             byte[] Bytes = new byte[passwordBytes.Length];
             string decryptedText;
-            int position = 3;
-            for (int i = 5; i < 10; i++)//encryptedData.Length
-            {
-                Bytes[i-5] = (byte)(encryptedData[i] ^ passwordBytes[i-5]);//% key.Length
-            }
-            Console.WriteLine(Encoding.UTF8.GetString(Bytes));
-            decryptedText = Decrypt(encryptedData, Bytes);
-            Console.WriteLine(decryptedText);
+            int position = 0;
             while (position <= encryptedData.Length - passwordBytes.Length)
             {
-                Console.WriteLine(position);
-                position++;
                 for (int i = 0; i < passwordBytes.Length; i++)//encryptedData.Length
                 {
-                    Bytes[i] = (byte)(encryptedData[i + position-1] ^ passwordBytes[i]);//% key.Length
+                    Bytes[i] = (byte)(encryptedData[i + position] ^ passwordBytes[i]);//% key.Length
                 }
                 decryptedText = Decrypt(encryptedData, Bytes);
                 if (decryptedText.Contains("Mit21"))
@@ -46,7 +37,7 @@ namespace laba2._3
                     Console.WriteLine("Дешифрований текст:");
                     Console.WriteLine(decryptedText);
                 }
-                //position++;
+                position++;
             }
         }
     }
--- laba2.3/obj/Debug/net8.0/laba2.3.AssemblyInfo.cs
+++ laba2.3/obj/Debug/net8.0/laba2.3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("laba2.3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b566e489afaa07215816dba34b084d4dd8d8b9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed6657b399c31622221a5d372c9db0d37732eeb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("laba2.3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("laba2.3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
