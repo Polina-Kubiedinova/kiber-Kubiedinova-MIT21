d9f90daf0456204a8fb103c5577738a903a680e1
polinakubiedinova <polinakubedino@gmail.com>
2024-09-26 22:48:52 +0300

    additional3

--- laba2.3/Program.cs
+++ laba2.3/Program.cs
@@ -17,17 +17,19 @@ namespace laba2._3
         }
         static void Main(string[] args)
         {
-            string encFileName = "C:\\polina\\yniver\\kiber\\laba2.3\\encfile5.dat";
+            string encFileName = "C:\\polina\\yniver\\kiber\\laba2.1\\data.dat";
+            //string encFileName = "C:\\polina\\yniver\\kiber\\laba2.3\\encfile5.dat";
             byte[] encryptedData = File.ReadAllBytes(encFileName);
             byte[] passwordBytes = Encoding.UTF8.GetBytes("Mit21");
-            byte[] Bytes = new byte[passwordBytes.Length];
+            //byte[] Bytes = new byte[passwordBytes.Length];
             string decryptedText;
             int position = 0;
-            while (position <= encryptedData.Length - passwordBytes.Length)
+            while (position < encryptedData.Length - passwordBytes.Length)
             {
+                byte[] Bytes = new byte[passwordBytes.Length];
                 for (int i = 0; i < passwordBytes.Length; i++)//encryptedData.Length
                 {
-                    Bytes[i] = (byte)(encryptedData[i + position] ^ passwordBytes[i]);//% key.Length
+                    Bytes[(i+position)%passwordBytes.Length] = (byte)(encryptedData[i+position] ^ passwordBytes[i]);//% key.Length
                 }
                 decryptedText = Decrypt(encryptedData, Bytes);
                 if (decryptedText.Contains("Mit21"))
